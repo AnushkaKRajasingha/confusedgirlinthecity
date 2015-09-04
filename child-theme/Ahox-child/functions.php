d0f5a0375f8e1d58ac9a420108af4f8f6770936a
AnushkaKRajasingha <anushkakrajasingha@gmail.com>
2015-09-04 17:26:01 +0530

    Closed #10 - Changes to the front page title
    Added wordings to the front page title and change the body text color to
    black .

--- child-theme/Ahox-child/functions.php
+++ child-theme/Ahox-child/functions.php
@@ -1,1 +1,6 @@
 <?php
+function ahox_childtheme_scripts() {	
+	wp_enqueue_script( 'script-ahox_childtheme',get_stylesheet_directory_uri() . '/js/script.js', array(), '1.0.0', true );
+}
+
+add_action( 'wp_enqueue_scripts', 'ahox_childtheme_scripts' );
