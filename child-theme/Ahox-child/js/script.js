ece0cb3fcce8f003e23851d3d1a3db8b2ef39658
AnushkaKRajasingha <anushkakrajasingha@gmail.com>
2015-09-05 08:26:05 +0530

    Fix for front page company wording and product price.
    - Can you make the text about the company bigger? You can't read it very
    well. Also, can we
    replace the words "Confused Girl" with the logo?
    
    - Remember, I said I didn't want the "$70-$72" to show up on the home
    page?
    I was able to add the $2 a couple weeks ago but didn't like that it
    shows up like "$70-$72".
    Is there anyway to have it look like "$70" on the homepage and then the
    extra $2 will be added?

--- child-theme/Ahox-child/js/script.js
+++ child-theme/Ahox-child/js/script.js
@@ -1,7 +1,14 @@
-(function($){
+(function($){	
 	$(document).ready(function(){
-		$var_c_wording = $('<h5/>').html('We are vulnerable because we are honest.  We are brave because we try.  We are Confused Girls.  Learning to embrace every part of ourselves. We take images of healing crystals and create active-wear that can only be described as pure art.  We support your journey in discovering your true nature. May our crystal inspired designs be a reminder of your eternal beauty.');
+		$var_c_wording = $('<h4/>').html('We are vulnerable because we are honest.  We are brave because we try.  We are Confused Girls.  Learning to embrace every part of ourselves. We take images of healing crystals and create active-wear that can only be described as pure art.  We support your journey in discovering your true nature. May our crystal inspired designs be a reminder of your eternal beauty.');
 		$('h1.page-title').html('Confused Girl').append($var_c_wording);
 		$('h1.page-title').show();
+		
+		$.each($('.price'),function(){
+			var $lowest_price = $(this).first().clone();
+			$(this).empty();
+			$(this).append($($lowest_price).children()[0]);
+		});
+		$('.price').show('slow');		
 	});
 })(jQuery);
